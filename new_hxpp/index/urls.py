b98d14ec136338020ad81f8981f11b7c90c1af1b
HXUbuntu <z3407279207@gmail.com>
2022-03-23 18:10:31 -0700

    注册页-迁移django自带模型类

--- new_hxpp/index/urls.py
+++ new_hxpp/index/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
-from .views import index
+from .views import *
 urlpatterns = [
-    path('index/',index.as_view(),name='index'),
+    path('',Regist.as_view(),name='register'),
+    path('login/',Login.as_view(),name='login'),
+    path('index/',Index.as_view(),name='index'),
+    path('logout/',LogoutUser.as_view(),name='logout')
 ]
--- new_hxpp/index/views.py
+++ new_hxpp/index/views.py
@@ -1,8 +1,60 @@
-from django.shortcuts import render
-from django.views import View
+from django.shortcuts import render,reverse,redirect
+from django.views.generic import View
+# 登录出错给出提示
 from django.http import HttpResponse
+# django自带用户模型下的表
+from django.contrib.auth.models import User
+# 登录和登出功能
+from django.contrib.auth import logout,login,authenticate
 # Create your views here.
 
-class index(View):
+# 注册
+class Regist(View):
     def get(self,request):
-        return HttpResponse('Hello world')
+        # 判断用户当前是否登录，如果登录就跳转到首页
+        if request.user.is_authenticated:
+            # 跳转
+            return redirect(reverse('index'))
+        return render(request,'register.html')
+
+    def post(self,request):
+        username = request.POST.get('username','')
+        password = request.POST.get('password','')
+        check_password = request.POST.get('check_password','')
+
+        if password != check_password:
+            return HttpResponse('密码输入不一致')
+
+        # 判断当前注册帐号是否已经被注册，如果已经被注册，则提示用户重新注册
+        exists = User.objects.filter(username=username).exists()
+        if exists:
+            return HttpResponse('该帐号已被注册')
+        User.objects.create_user(username=username,password=password)
+        return redirect(reverse('login'))
+
+
+# 登录
+class Login(View):
+    def get(self, request):
+        return render(request,'login.html')
+
+    def post(self, request):
+        pass
+
+
+# 网站首页
+class Index(View):
+    def get(self, request):
+        pass
+
+    def post(self, request):
+        pass
+
+
+# 用户注销
+class LogoutUser(View):
+    def get(self, request):
+        pass
+
+    def post(self, request):
+        pass
