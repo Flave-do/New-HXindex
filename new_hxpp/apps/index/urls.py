62e5d9f26529657e3306102a4cf6b462ee812c1f
HXUbuntu <z3407279207@gmail.com>
2022-03-28 00:06:13 -0700

    多页面，不懂继承直接复制

--- new_hxpp/apps/index/urls.py
+++ new_hxpp/apps/index/urls.py
@@ -6,5 +6,11 @@ urlpatterns = [
     path('index/',Index.as_view(),name='index'),
     path('logout/',LogoutUser.as_view(),name='logout'),
     path('sureg/',Sureg.as_view(),name='sureg'),
-    path('info/',Info.as_view(),name='info'),
+    path('info/',Info.as_view(),name='info1'),
+    path('info2/',Info.as_view(),name='info2'),
+    path('info3/',Info.as_view(),name='info3'),
+    path('info4/',Info.as_view(),name='info4'),
+    path('info5/',Info.as_view(),name='info5'),
+    path('info6/',Info.as_view(),name='info6'),
+    path('info7/',Info.as_view(),name='info7'),
 ]
